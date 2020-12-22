c4352fdd11f0a5dfa592489c369e154e5b783adf
masutaro99 <maskenpa1001@gmail.com>
2020-12-22 22:00:55 +0900

    create set_target_board2

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,6 @@
 class BoardsController < ApplicationController
+  before_action :set_target_board, only: %i[show edit update destroy]
+
   def index
     @boards = Board.all
   end
@@ -10,19 +12,15 @@ class BoardsController < ApplicationController
     redirect_to board
   end
   def show
-    @board = Board.find_by(id: params[:id])
   end
   def edit
-    @board = Board.find_by(id: params[:id])
   end
   def update
-    board = Board.find_by(id: params[:id])
-    board.update(board_params)
+    @board.update(board_params)
     redirect_to(board_path)
   end
   def destroy
-    board = Board.find_by(id: params[:id])
-    board.delete
+    @board.delete
     redirect_to boards_path
   end
 
@@ -30,5 +28,8 @@ class BoardsController < ApplicationController
 
   def board_params
     params.require(:board).permit(:name, :title, :body)
+  end
+  def set_target_board
+    @board = Board.find(params[:id])
   end
 end
