8b5e6c997108d5742074bc9d7da6f3ceb6e4a077
masutaro99 <maskenpa1001@gmail.com>
2020-12-26 23:09:22 +0900

    create sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get '/boards', to: 'boards#index'
   # get '/boards/new', to: 'boards#new'
