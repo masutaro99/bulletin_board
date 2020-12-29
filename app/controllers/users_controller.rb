3b38a54abbdadba1959f5f0e77a2dffd766c101f
masutaro99 <maskenpa1001@gmail.com>
2020-12-29 15:44:56 +0900

    add users post test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_to boards_path
+      redirect_to mypage_path
     else
     # :backを渡して1つ前のページに戻る
       redirect_to :back, flash: {
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /users/new" do
+  describe "GET #new" do
     before (:each) do
       get new_user_path
     end
@@ -15,5 +15,23 @@ RSpec.describe "Users", type: :request do
       expect(assigns(:user)).to be_a_new User
     end
   end
-
+  
+  describe "POST #create" do
+    context "正しいユーザー情報が渡って来た場合" do
+      let(:params) do
+        { user: {
+            name: "user",
+            password: "password",
+            password_confirmation: "password"
+          }
+        }
+      end
+      it "ユーザーが一人増えていること" do
+        expect { post users_path, params: params }.to change(User, :count).by(1) 
+      end
+      it "マイページにリダイレクトされること" do
+        expect(post users_path, params: params).to redirect_to(mypage_path)
+      end
+    end
+  end
 end
