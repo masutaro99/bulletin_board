ddf7a22e144bb74df5bedf5b39a06959bfbab383
masutaro99 <maskenpa1001@gmail.com>
2021-01-04 20:53:09 +0900

    投稿を削除した後、投稿一覧ページにいくようにした、usersをリソースベースルーティングに

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,16 @@
 Rails.application.routes.draw do
   get 'contacts/new'
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/me'
-
+  
   get "mypage", to: "users#me"
+
+  # session管理系
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # get '/boards', to: 'boards#index'
-  # get '/boards/new', to: 'boards#new'
-  # post 'boards', to: 'boards#create'
-  # get 'boards/:id', to: 'boards#show'
+  # リソースベースルーティングで一括設定
   root "home#index"
   resources :users, only: %i[new create]
   resources :boards
   resources :comments, only: %i[create destroy]
+  resources :users, only: %i[new create]
 end
