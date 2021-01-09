0cc15f31f5823d1acee40a15136d13db96d59298
masutaro99 <maskenpa1001@gmail.com>
2021-01-09 22:00:40 +0900

    to path test

--- db/migrate/20210109071005_set_status_default_to_contact.rb
+++ db/migrate/20210109071005_set_status_default_to_contact.rb
@@ -1,5 +1,5 @@
 class SetStatusDefaultToContact < ActiveRecord::Migration[5.2]
   def change
-    change_column :contacts, :status, :text, default => "未対応"
+    change_column :contacts, :status, :text
   end
 end
